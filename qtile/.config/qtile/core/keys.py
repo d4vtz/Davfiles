0cb7165b6401bf80a037507c939ffd30f96b7941
medicendav <davidtorrezreyes@gmail.com>
2022-11-25 17:50:44 -0600

    REFACTOR: Group utility functions

--- qtile/.config/qtile/core/keys.py
+++ qtile/.config/qtile/core/keys.py
@@ -2,7 +2,7 @@ from libqtile.config import Key
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 import os
-import subprocess
+from utils.functions import resize_down, resize_left, resize_right, resize_up, backlight 
 
 mod = "mod4"
 control = "control"
@@ -10,95 +10,6 @@ shift = "shift"
 alt = "mod1"
 terminal = guess_terminal()
 home = os.path.expanduser('~')
-
-# resize functions
-def resize(qtile, direction):
-    layout = qtile.current_layout
-    child = layout.current
-    parent = child.parent
-
-    while parent:
-        if child in parent.children:
-            layout_all = False
-
-            if (direction == "left" and parent.split_horizontal) or (
-                direction == "up" and not parent.split_horizontal
-            ):
-                parent.split_ratio = max(5, parent.split_ratio - layout.grow_amount)
-                layout_all = True
-            elif (direction == "right" and parent.split_horizontal) or (
-                direction == "down" and not parent.split_horizontal
-            ):
-                parent.split_ratio = min(95, parent.split_ratio + layout.grow_amount)
-                layout_all = True
-
-            if layout_all:
-                layout.group.layout_all()
-                break
-
-        child = parent
-        parent = child.parent
-
-
-@lazy.function
-def resize_left(qtile):
-    current = qtile.current_layout.name
-    layout = qtile.current_layout
-    if current == "bsp":
-        resize(qtile, "left")
-    elif current == "columns":
-        layout.cmd_grow_left()
-
-
-@lazy.function
-def resize_right(qtile):
-    current = qtile.current_layout.name
-    layout = qtile.current_layout
-    if current == "bsp":
-        resize(qtile, "right")
-    elif current == "columns":
-        layout.cmd_grow_right()
-
-
-@lazy.function
-def resize_up(qtile):
-    current = qtile.current_layout.name
-    layout = qtile.current_layout
-    if current == "bsp":
-        resize(qtile, "up")
-    elif current == "columns":
-        layout.cmd_grow_up()
-
-
-@lazy.function
-def resize_down(qtile):
-    current = qtile.current_layout.name
-    layout = qtile.current_layout
-    if current == "bsp":
-        resize(qtile, "down")
-    elif current == "columns":
-        layout.cmd_grow_down()
-
-
-def backlight(action):
-    def f(qtile):
-        brightness = int(subprocess.run(['brightnessctl', 'g'], stdout=subprocess.PIPE).stdout)
-        max_brightness = int(subprocess.run(['brightnessctl', 'm'], stdout=subprocess.PIPE).stdout)
-        step = int(max_brightness / 10)
-
-        if action == 'inc':
-            if brightness < max_brightness - step:
-                subprocess.run(['brightnessctl', 'set', str(brightness + step)], stdout=subprocess.PIPE).stdout
-            else:
-                subprocess.run(['brightnessctl', 'set', str(max_brightness)], stdout=subprocess.PIPE).stdout
-        elif action == 'dec':
-            if brightness > step:
-                subprocess.run(['brightnessctl', 'set', str(brightness - step)], stdout=subprocess.PIPE).stdout
-            else:
-                subprocess.run(['brightnessctl', 'set', '0'], stdout=subprocess.PIPE).stdout
-    return f
-
-from libqtile import layout
 keys = [
     # essentials
     Key([mod], "y", lazy.layout.hide()),
