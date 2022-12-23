c0c960ce776a330864b1f7b4fcf0fd722b9337cc
David Torrez <davidtorrezreyes@gmai.com>
2022-12-23 00:06:53 -0600

    FEAT: Add albert to autostart file

--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -12,5 +12,5 @@ xfce4-power-manager &
 mkfifo /tmp/vol-icon && ~/.config/qtile/scripts/vol_icon.sh &
 dbus-update-activation-environment DISPLAY XAUTHORITY WAYLAND_DISPLAY
 dbus-update-activation-environment --all
-
+albert &
 # setxkbmap -layout us,us -variant ,intl -option grp:ctrl_shift_toggle &
