0e7a36b7a00d47655bccd207401f46df938b9e56
David Torrez <davidtorrezreyes@gmai.com>
2023-01-22 12:03:47 -0600

    ADD: Add latex snippets

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,24 +1,25 @@
 -- set vim options here (vim.<first_key>.<second_key> =  value)
 return {
-	opt = {
-		relativenumber = true, -- sets vim.opt.relativenumber
-		conceallevel = 2, -- enable conceal
-		linebreak = true, -- linebreak soft wrap at words
-		list = true, -- show whitespace characters
-		listchars = {
-			tab = "│→",
-			extends = "⟩",
-			precedes = "⟨",
-			trail = "·",
-			nbsp = "␣",
-			eol = "↲",
-		},
-		showbreak = "↪ ",
-		wrap = true, -- soft wrap lines
-		spellfile = "~/.config/astronvim/lua/user/spell/es.utf-8.add",
-		thesaurus = "~/.config/astronvim/lua/user/spell/mthesaur.txt",
-	},
-	g = {
-		mapleader = " ", -- sets vim.g.mapleader
-	},
+  opt = {
+    relativenumber = true, -- sets vim.opt.relativenumber
+    conceallevel = 2, -- enable conceal
+    linebreak = true, -- linebreak soft wrap at words
+    list = true, -- show whitespace characters
+    listchars = {
+      tab = "│→",
+      extends = "⟩",
+      precedes = "⟨",
+      trail = "·",
+      nbsp = "␣",
+      eol = "↲",
+    },
+    showbreak = "↪ ",
+    wrap = true, -- soft wrap lines
+    spellfile = "~/.config/astronvim/lua/user/spell/es.utf-8.add",
+    thesaurus = "~/.config/astronvim/lua/user/spell/mthesaur.txt",
+    swapfile = false,
+  },
+  g = {
+    mapleader = " ", -- sets vim.g.mapleader
+  },
 }
--- nvim/.config/nvim/lua/user/plugins/cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/cmp.lua
@@ -1,4 +1,8 @@
 return {
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+		keyword_length = 2,
+	},
 	source_priority = {
 		luasnip = 1000,
 		nvim_lua = 800,
