892e62c9d814f8a52f5726ea98d2ed431608ab7e
David Torrez <davidtorrezreyes@gmail.com>
2023-03-11 20:52:18 -0600

    ADD: Configs for vimtex

--- nvim/.config/nvim/lua/user/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/user/plugins/vimtex.lua
@@ -3,9 +3,9 @@ return {
 	config = function()
 		-- vim.g.vimtex_compiler_engine = "lualatex"
 		vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_quickfix_enabled = 0
-		vim.g.vimtex_quickfix_mode = 2
-		vim.g.vimtex_quickfix_open_on_warning = 0
+		-- vim.g.vimtex_quickfix_enabled = 1
+		-- vim.g.vimtex_quickfix_mode = 2
+		-- vim.g.vimtex_quickfix_open_on_warning = 1
 		vim.g.vimtex_compiler_latexmk = {
 			build_dir = "Build",
 			callback = 1,
