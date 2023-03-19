686d6c72584f2c4c15f67eba7ee43c00558741e0
David Torrez <davidtorrezreyes@gmail.com>
2023-03-19 12:24:33 -0600

    ADD: Add extension telescope_file_browser

---
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -0,0 +1,56 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
+  },
+  cmd = "Telescope",
+  opts = function()
+    local actions = require "telescope.actions"
+    local get_icon = require("astronvim.utils").get_icon
+    require("telescope").load_extension "file_browser"
+    return {
+      defaults = {
+        prompt_prefix = string.format("%s ", get_icon "Search"),
+        selection_caret = string.format("%s ", get_icon "Selected"),
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = { ["q"] = actions.close },
+        },
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+      },
+    }
+  end,
+}
