598bba897ae1dc67fa1cab293b788831afdcce61
D4vtz <davidtorrezreyes@gmail.com>
2024-03-23 19:23:01 -0600

    FIX: Arreglar flags de chrome y eliminar funcion zsh tmux

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,9 +1,3 @@
 function mkcd(){
   mkdir -p $1 && cd $1
 }
-
-tmux-window-name() {
-	($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
-}
-
-add-zsh-hook chpwd tmux-window-name
